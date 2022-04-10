218d2622d5544967d7d561f1eeee2ee9061716a6
SadeghPouriyanZadeh <pouriyan.sadegh@gmail.com>
2022-04-11 00:56:50 +0430

    change title of about page and home page change body of home page add context arg to home view

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
+
+posts = [
+    {
+        "author": "Corey Schafer",
+        "title": "Blog Post 1",
+        "content": "First post content",
+        "date_posted": "April 20, 2018",
+    },
+    {
+        "author": "Jane Doe",
+        "title": "Blog Post 2",
+        "content": "Second post content",
+        "date_posted": "April 21, 2018",
+    },
+]
 # Create your views here.
 def home(request):
-    return render(request, "blog/home.html")
+    context = {
+        "posts": posts,
+    }
+    return render(request, "blog/home.html", context)
 
 
 def about(request):
-    return render(request, "blog/about.html")
+    return render(request, "blog/about.html", {"title": "About"})
