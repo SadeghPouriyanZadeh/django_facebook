c588d6e260c872020bed4094d069c8bd95f8972f
SadeghPouriyanZadeh <pouriyan.sadegh@gmail.com>
2022-04-11 03:22:15 +0430

    remove dummy data in views

--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,6 @@
 from django.shortcuts import render
 from .models import Post
 
-posts = [
-    {
-        "author": "Corey Schafer",
-        "title": "Blog Post 1",
-        "content": "First post content",
-        "date_posted": "April 20, 2018",
-    },
-    {
-        "author": "Jane Doe",
-        "title": "Blog Post 2",
-        "content": "Second post content",
-        "date_posted": "April 21, 2018",
-    },
-]
-
 
 def home(request):
     context = {
