002c324a632df5d2d06d21a6545cd1c175e25385
SadeghPouriyanZadeh <pouriyan.sadegh@gmail.com>
2022-04-11 00:29:40 +0430

    add templates add blog app to settings add secret key to settings because this repo is for showing my skills in django

--- blog/views.py
+++ blog/views.py
@@ -3,4 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse(request, "Hello, world. You're at the blog home.")
+    return render(request, "blog/home.html")
+
+
+def about(request):
+    return render(request, "blog/about.html")
--- django_facebook/settings.py
+++ django_facebook/settings.py
@@ -20,6 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-t6va=r3v#)v)ug^adlt&8ip3lpqczr_d*fp+(0!26lcrbfe%+x"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -36,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "blog",
+    "blog.apps.BlogConfig",
 ]
 
 MIDDLEWARE = [
