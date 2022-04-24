f7acf38069663c7fd0e5488f5fcac82be4a1bd5e
SadeghPouriyanZadeh <pouriyan.sadegh@gmail.com>
2022-04-24 16:30:38 +0430

    create 1 file and update 2 files

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Post
-
+from django.views.generic import ListView, DetailView
 
 def home(request):
     context = {
@@ -11,3 +11,12 @@ def home(request):
 
 def about(request):
     return render(request, "blog/about.html", {"title": "About"})
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html' # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
